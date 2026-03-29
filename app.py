e817cbf9e86cd8c7dcb5a897e08648857c6be329
MPHelsinkiUni <dat.t.vo@helsinki.fi>
2026-03-29 22:42:22 +0300

    All base functions have been added, though jank is still present. Adding, removing, modifying, viewing and searching for reviews implemented.

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import sqlite3
 from flask import Flask
 from flask import redirect, render_template, request, session
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime, timezone
 import db
 import config
 import items
@@ -12,8 +11,13 @@ app.secret_key = config.secret_key
 
 @app.route("/") # Root
 def index():
-    words = ["Newest movie #1", "Newest movie #2", "Newest movie #3"]
-    return render_template("index.html", message = "Welcome to film review site!", items = words)
+    reviews = items.get_items()
+    return render_template("index.html", message = "Welcome to film review site!", items = reviews)
+
+@app.route("/item/<int:item_id>")
+def show_item(item_id):
+    item = items.get_item(item_id)
+    return render_template("review_data.html", item=item)
 
 @app.route("/page1")
 def page1():
@@ -33,16 +37,16 @@ def create():
     password1 = request.form["password1"]
     password2 = request.form["password2"]
     if password1 != password2:
-        return "VIRHE: salasanat eivät ole samat. Tarkistaa salasanat uudestaan"
+        return "Warning: Passwords were not the same. Please double check your inputs"
     password_hash = generate_password_hash(password1)
 
     try:
         sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
         db.execute(sql, [username, password_hash])
     except sqlite3.IntegrityError:
-        return "VIRHE: tunnus on jo varattu"
+        return "Warning: Your username has already been chosen. Please pick another one."
 
-    return "Tunnus luotu"
+    return render_template("registration_success.html")
 
 ###################
 # This section manages logins, and logouts
@@ -67,7 +71,7 @@ def login():
             session["username"] = username
             return redirect("/")
         else:
-            return "VIRHE: väärä tunnus tai salasana"
+            return "Warning: Double-check your username or password."
 
 @app.route("/logout")
 def logout():
@@ -104,33 +108,57 @@ def insert_review():
 
     return redirect("/")
 
+@app.route("/edit_review", methods = ["POST"])
+def edit_review_auxiliary():
+    # Input names are: title, review_body, stars, work, imdb_snippet (in HTML page). Linked as such
+    # title -> title
+    # session.username -> poster
+    # session.user_id -> poster_id
+    # review_body -> review_body
+    # stars -> stars
+    # work -> work
+    # work_id snippet will be worked on later. (WIP!!!)
+    # imdb_snippet -> imdb_snippet
+    item_id = request.form["item_id"]
+    title = request.form["title"]
+    review_body = request.form["review_body"]
+    stars = int(request.form["stars"])
+    work = request.form["work"]
+    imdb_snippet = request.form["imdb_snippet"]
+
+    items.update_item(item_id, title, review_body, stars, work, imdb_snippet)
+
+    return redirect("/item/"+str(item_id))
+
+@app.route("/edit_review/<int:item_id>")
+def edit_review(item_id):
+    item = items.get_item(item_id)
+    return render_template("edit_review.html", item=item)
+
+@app.route("/remove_review/<int:item_id>", methods=["GET", "POST"])
+def remove_review(item_id):
+    if request.method == "GET":
+        item = items.get_item(item_id)
+        return render_template("remove_review.html", item=item)
+
+    if request.method == "POST":
+        if "remove" in request.form:
+            items.remove_item(item_id)
+            return redirect("/")
+        else:
+            return redirect("/item/" + str(item_id))
+
 #################### 
-# Debugger below
-@app.route("/debugfor")
-def debugfor():
-    content = ""
-    for i in range(1, 101):
-        content += str(i) + " "
-    return content
-
-@app.route("/debug/page/<int:page_id>")
-def debugpage(page_id):
-    return "Tämä on sivu " + str(page_id)
-
-@app.route("/debug/form")
-def form():
-    return render_template("form.html")
-
-@app.route("/debug/order")
-def order():
-    return render_template("debug_order.html")
-
-@app.route("/debug/pizza/result", methods=["POST"])
-def result():
-    pizza = request.form["pizza"]
-    extras = request.form.getlist("extra")
-    message = request.form["message"]
-    return render_template("result.html", pizza=pizza, extras=extras, message=message)
+# This section manages search functionality
+@app.route("/search_review")
+def search_review():
+    query = request.args.get("query")
+    if query:
+        results = items.search_review(query)
+    else:
+        query = ""
+        results = []
+    return render_template("search_review.html", query=query, results=results)
 
 # Check on 127.0.0.1:5000 or localhost:5000
 # Procedure: `source ./venv/bin/activate` -> `flask run` -> check -> CTRL+C -> `deactivate`
--- items.py
+++ items.py
@@ -1,7 +1,53 @@
 import db
+import datetime
 
 def add_item(title, username, user_id, review_body, stars, work, imdb_snippet):
-    sql = """INSERT INTO reviews (title, poster, poster_id, review_body, stars, work, imdb_snippet) 
-            VALUES (?, ?, ?, ?, ?, ?, ?)"""
+    sql = """INSERT INTO reviews (title, poster, poster_id, review_body, stars, work, time_posted, imdb_snippet) 
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)"""
+    time_posted = datetime.datetime.now()
     
-    db.execute(sql, [title, username, user_id, review_body, stars, work, imdb_snippet])
+    db.execute(sql, [title, username, user_id, review_body, stars, work, time_posted, imdb_snippet])
+
+def get_items():
+    sql = """SELECT id, title, poster, stars, imdb_snippet FROM reviews ORDER BY time_posted DESC LIMIT 5"""
+    return db.query(sql)
+    # Ideally, there should be work as well, but for the purposes of debugging, we do not do this.
+
+def get_item(item_id):
+    sql = """SELECT reviews.id,
+                    reviews.title,
+                    reviews.poster,
+                    reviews.poster_id,
+                    reviews.review_body,
+                    reviews.stars,
+                    reviews.work,
+                    reviews.time_posted,
+                    reviews.imdb_snippet,
+                    users.id user_id,
+                    users.username,
+                    users.assignment_role
+             FROM reviews, users
+             WHERE reviews.poster_id = users.id AND
+                   reviews.id = ?"""
+    return db.query(sql, [item_id])[0]
+
+def update_item(item_id, title, review_body, stars, work, imdb_snippet):
+    sql = """UPDATE reviews SET title = ?,
+                                review_body = ?,
+                                stars = ?,
+                                work = ?,
+                                imdb_snippet = ?
+                            WHERE id = ?"""
+    db.execute(sql, [title, review_body, stars, work, imdb_snippet, item_id])
+
+def remove_item(item_id):
+    sql = """DELETE FROM reviews WHERE id = ?"""
+    db.execute(sql, [item_id])
+
+def search_review(query):
+    sql = """SELECT id, title, poster 
+             FROM reviews
+             WHERE title LIKE ? OR review_body LIKE ?
+             ORDER BY time_posted DESC"""
+    x = "%" + query + "%"
+    return db.query(sql, [x, x])
